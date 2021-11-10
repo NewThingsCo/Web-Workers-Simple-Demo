e9c810120a9f0e8424f0526f2959f3b12cd5766c
Oliver Lillie <oliver.lillie@newthings.co>
2021-11-10 22:11:05 +0200

    const vs var

--- longRunningTask.js
+++ longRunningTask.js
@@ -1,24 +1,24 @@
-var prime = (num) => {
-    for (var i = 0; i <= num; i++) {
-        for (var j = 2; j < i; j++) {
-            if (i % j == 0) {
+const prime = (num) => {
+    for(let i = 0; i <= num; i++) {
+        for(let j = 2; j < i; j++) {
+            if(i % j === 0) {
                 break;
             }
         }
     }
 };
 
-var disableElm = function(elm) {
+const disableElm = function(elm) {
     elm.classList.add('spinner');
     elm.setAttribute('disabled', 'disabled');
-}
+};
 
-var enableElm = function(elm) {
+const enableElm = function(elm) {
     elm.classList.remove('spinner');
     elm.removeAttribute('disabled');
-}
+};
 
-var runHeavyTaskMainThread = function(elm) {
+const runHeavyTaskMainThread = function(elm) {
     disableElm(elm);
     // setTimeout exists to allow the button a chance to change
     // otherwise that too is blocked by the prime calculations
@@ -28,7 +28,11 @@ var runHeavyTaskMainThread = function(elm) {
     }, 100)
 };
 
-var runHeavyTaskWorkerThread = function(elm) {
+const runHeavyTaskWorkerThread = function(elm) {
+    if(!window.Worker) {
+        alert('Web workers are not supported by your browser.')
+        return;
+    }
     disableElm(elm);
     var worker = new Worker("./worker.js");
     worker.postMessage(200000);
