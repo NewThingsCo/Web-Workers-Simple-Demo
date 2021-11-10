2adbb186da07746c3b8223e0e58d31f3f65048a9
Oliver Lillie <oliver.lillie@newthings.co>
2021-11-10 22:16:32 +0200

    const vs var

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
-var prime = (num) => {
-    for (var i = 0; i <= num; i++) {
-        for (var j = 2; j < i; j++) {
-            if (i % j == 0) {
+const intensive = (num) => {
+    for(let i = 0; i <= num; i++) {
+        for(let j = 2; j < i; j++) {
+            if(i % j === 0) {
                 break;
             }
         }
